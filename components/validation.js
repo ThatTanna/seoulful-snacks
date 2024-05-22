6bd4f39e9304ff22b12824e7b83f5a1e4dce3139
tayshialyn <tayshialyn@gmail.com>
2024-05-22 18:42:13 +0800

    Added login functionality

--- components/validation.js
+++ components/validation.js
@@ -17,3 +17,40 @@
         }, false)
     })
 })()
+
+
+async function logIn({email, password, api}){
+
+    const request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    };
+
+    // send email and password to server
+    const response = await fetch(api, request);
+    const status = response.status;
+    const data = await response.json();
+
+    // Return the result only if the status is 200 (OK), else return false
+    return status === 200 ? data : false;
+
+}
+
+function showToast({toastElement, toastBodyElement, bgColor, msg}){
+    // Run BootStrap5's toast to show the activity is complete.
+    const toastEl = toastElement;
+    const toastBody = toastBodyElement;
+    toastEl.classList.remove("bg-success"); //remove all known and used colors here first
+    toastEl.classList.remove("bg-danger");  //remove all known and used colors here first
+    toastEl.classList.add(`bg-${bgColor}`);
+    toastEl.classList.add("text-white");
+    toastBody.textContent = msg;
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
+}
