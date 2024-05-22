6355fc6649dc1cd5b74c596acb396b78120d1c0d
tayshialyn <tayshialyn@gmail.com>
2024-05-22 19:58:45 +0800

    Added signup functionality. Moved login validation to login.js

--- components/validation.js
+++ components/validation.js
@@ -19,29 +19,8 @@
 })()
 
 
-async function logIn({email, password, api}){
 
-    const request = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    };
-
-    // send email and password to server
-    const response = await fetch(api, request);
-    const status = response.status;
-    const data = await response.json();
-
-    // Return the result only if the status is 200 (OK), else return false
-    return status === 200 ? data : false;
-
-}
-
+// Toast functionality for validation or form submission
 function showToast({toastElement, toastBodyElement, bgColor, msg}){
     // Run BootStrap5's toast to show the activity is complete.
     const toastEl = toastElement;
