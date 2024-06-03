d9e88dc32b643d90675a46e69548a66bcef5bbda
tayshialyn <tayshialyn@gmail.com>
2024-06-03 21:02:15 +0800

    Added logic for showing subscription from backend on profile page

--- components/profile.js
+++ components/profile.js
@@ -22,6 +22,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    async function getUserSubscription({ currentLoggedInUserEmail, api }) {
+        var url = new URL(api + "/" + currentLoggedInUserEmail);
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("token")}`
+            }
+        }
+        );
+        const status = response.status;
+        const data = await response.json();
+
+        // Return the result only if the status is 200 (OK), else return false
+        return status === 200 ? data : false;
+
+    }
+
     async function fetchData() {
         // Read the current logged in user from access token JWT stored in localstorage and decode it to get the email
         // https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
@@ -35,14 +52,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
         var currentLoggedInUserEmail = JSON.parse(jsonPayload).sub
 
-        const api = "http://localhost:8080/profile/api/user";
-        const result = await getUser({ currentLoggedInUserEmail, api });
-        
+        // Get profile
+        var api = "http://localhost:8080/profile/api/user";
+        var result = await getUser({ currentLoggedInUserEmail, api });
+
         if (result) { // If there's valid result
             console.log(result);
             document.getElementById('name').value = result.name;
             document.getElementById('email').value = result.email;
             document.getElementById('billingAddress').value = result.billingAddress;
+        }
+
+        // Get subscriptions
+        api = "http://localhost:8080/user/api/subscription";
+        result = await getUserSubscription({ currentLoggedInUserEmail, api });
+
+        if (result) { // If there's valid result
+            console.log(result);
+            // Show and hide elements depending on number of subscriptions
+            if (!result || result.length == 0) {
+                document.getElementById("subscription-none").style.display = 'flex';
+                document.getElementById("subscription-exists").style.display = 'none';
+            } else { // If got more than one subscription
+                document.getElementById("subscription-none").style.display = 'none';
+                document.getElementById("subscription-exists").style.display = 'flex';
+                var subscriptionsHTMLelements = ''+
+                '<div class="container m-3">'+
+                    '<div class="row">'
+                    for (let i = 0; i < result.length; i++) {
+                        subscriptionsHTMLelements += ''+
+                        '<div class="col-md-12">'+
+                            '<span class="text-secondary ms-3">Subscription period: '+new Date(result[i].created_on).toISOString().split('T')[0]+' to '+new Date(result[i].expired_on).toISOString().split('T')[0]+'</span><br />'+
+                            '<div class="border rounded-3 p-2 mb-4 d-flex align-items-center justify-content-between">'+
+                                '<div class="d-flex align-items-center">'+
+                                    '<img class="border rounded me-2" width="100px" src="'+"http://localhost:8080"+result[i].product.imagePath+'">'+
+                                    '<div class="">Subscription '+(i+1)+': <span class="fw-bold">'+result[i].product.name+'</span>'+
+                                    '</div>'+
+                                '</div>'+
+                                '<div class="d-flex align-items-center">'+
+                                '<div class="m-4 fw-bold">Qty: '+result[i].quantity+'</div>'+
+                                '</div>'+
+                            '</div>'+
+                        '</div>'
+                    }
+
+                    subscriptionsHTMLelements += ''+
+                    '</div>'+
+                '</div>'
+                $('#subscription-exists').append(subscriptionsHTMLelements);
+            }
         }
     }
 
